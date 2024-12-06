cdd3155f9fb56f9899ef636f07bc20172f7192a7
Excell Christian <excellchriatian12@gmail.com>
2024-12-06 13:39:57 +0700

    Update2 Jobsheet15_Group1.java

--- Jobsheet15_Group1.java
+++ Jobsheet15_Group1.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Jobsheet15_Group1 {
     public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+    String studentName, nim, courseCode, courseName;
         System.out.println("=== Sistem Pemantauan KRS Mahasiswa ===");
-       
     }
 }
