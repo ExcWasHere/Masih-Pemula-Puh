b74e074af000dd5aaae76be6a962d56b7f4cc156
WahyuFairuzD <wahyufairuz05@gmail.com>
2024-12-06 13:34:56 +0700

    Update Jobsheet15_Group1.java

--- Jobsheet15_Group1.java
+++ Jobsheet15_Group1.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Jobsheet15_Group1 {
     public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+        System.out.println("=== Sistem Pemantauan KRS Mahasiswa ===");
        
     }
 }
