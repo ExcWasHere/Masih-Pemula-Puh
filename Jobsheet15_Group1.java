bc609de3d933472b82585a710219881ab8d59344
Watash <wahyufairuz05@gmail.com>
2024-12-06 13:45:40 +0700

    update 1

--- Jobsheet15_Group1.java
+++ Jobsheet15_Group1.java
@@ -3,6 +3,15 @@ public class Jobsheet15_Group1 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String studentName, nim, courseCode, courseName;
+
         System.out.println("=== Sistem Pemantauan KRS Mahasiswa ===");
+        System.out.println("1. Input Data KRS");
+        System.out.println("2. Tampilkan Daftar KRS Mahasiswa ");
+        System.out.println("3. Analisis Data KRS");
+        System.out.println("4. Keluar");
+        
+        
+
+
     }
 }
