d5f48170eda2c17cce635780468648e170625744
Exc <excellchriatian12@gmail.com>
2024-12-06 13:54:07 +0700

    Update2

--- Jobsheet15_Group1.java
+++ Jobsheet15_Group1.java
@@ -4,12 +4,13 @@ public class Jobsheet15_Group1 {
     Scanner sc = new Scanner(System.in);
     String studentName, nim, courseCode, courseName;
 
+    while(true){
         System.out.println("=== Sistem Pemantauan KRS Mahasiswa ===");
         System.out.println("1. Input Data KRS");
         System.out.println("2. Tampilkan Daftar KRS Mahasiswa ");
         System.out.println("3. Analisis Data KRS");
         System.out.println("4. Keluar");
-        
+    }
         
 
 
